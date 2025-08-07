0be8bde7de8241ca36967568bbc30ff47b7ed258
Shagun Prakash Hegde <shagun@tizzl.app>
2025-08-08 00:18:01 +0530

    fix import statements pt5

--- tizzl/models/__init__.py
+++ tizzl/models/__init__.py
@@ -1,6 +1,6 @@
-from tizzl.models.product import Product, ProductAttribute, ProductEmbedding, Category, Occasion, Season
-from tizzl.models.user import UserProfile, StylePreference, UserQuery, BodyType, StylePersonality
-from tizzl.models.response import StylistResponse, OutfitRecommendation, OutfitItem
+from .product import Product, ProductAttribute, ProductEmbedding, Category, Occasion, Season
+from .user import UserProfile, StylePreference, UserQuery, BodyType, StylePersonality
+from .response import StylistResponse, OutfitRecommendation, OutfitItem
 
 __all__ = [
     'Product', 
